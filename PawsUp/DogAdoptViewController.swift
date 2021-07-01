d3a2f1c5563b2b5c79a116ea5e3f94c941ba0569
Erin Martin <ecm8807@gmail.com>
2021-07-01 14:55:39 -0400

    End of Day 9

---
+++ PawsUp/DogAdoptViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DogAdoptViewController.swift
+//  PawsUp
+//
+//  Created by Erin Martin on 7/1/21.
+//
+
+import UIKit
+import WebKit
+
+class DogAdoptViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   //    let dogURL = URL(string:"  https://www.petfinder.com/search/dogs-for-adoption/us/nc/cary/")
+    //    let myRequest = URLRequest(url: dogURL!)
+      //  animalAdoptWV.load(myRequest)
+    }
+    
+}
--- PawsUp/ViewController.swift
+++ PawsUp/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet var dogWV: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+    
+    let myURL = URL(string:"  https://www.petfinder.com/search/dogs-for-adoption/us/nc/cary/")
+    let myRequest = URLRequest(url: myURL!)
+    dogWV.load(myRequest)
+}
 }
 
